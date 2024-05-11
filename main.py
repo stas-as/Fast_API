fcdc6b3e6be81bcecc3b333b3d4fc3a3c1524b86
stas-as <124561396+stas-as@users.noreply.github.com>
2024-05-11 20:39:03 +0300

    1

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-
-def hello():
-    return "Hello world"
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
