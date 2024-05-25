9e3c92257d40102d2d6913bb3df7453d1141d511
stas-as <124561396+stas-as@users.noreply.github.com>
2024-05-25 20:28:59 +0300

    1

--- app/main.py
+++ app/main.py
@@ -4,5 +4,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/hotels")
-def get_hotels():
+def get_hotels(
+    locations,
+    date_from,
+    date_to,
+    stars,
+):
     return "Отель Бридж Резорт 5 звезд"
